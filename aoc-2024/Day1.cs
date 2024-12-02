cf988571a2e3f3099298dcf2dd9646d77e4b4b3b
Clayton.Safranek <Clayton.Safranek@kiewit.com>
2024-12-02 13:54:49 -0600

    day 1

---
+++ aoc-2024/Day1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc_2024
+{
+    public class Day1
+    {
+        static string[] ReadInputFromFile(string filePath)
+        {
+            return File.ReadAllLines(filePath);
+        }
+
+        public void EvaluateInput()
+        {
+            var answer = 0;
+            var similarityScore = 0;
+            var day1Input = ReadInputFromFile("C:\\\\Users\\\\Clayton.Safranek\\\\Documents\\\\projects\\\\aoc-2024\\\\aoc-2024\\\\day1-input.txt");
+
+            var locationList1 = new List<int>();
+            var locationList2 = new List<int>();
+            var numberCounts = new Dictionary<int, int>();
+
+            for (int i = 0; i < day1Input.Length; i++)
+            {
+                // separate into individual lists
+                var numbers = day1Input[i].Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                if (int.TryParse(numbers[0], out int number1) && int.TryParse(numbers[1], out int number2))
+                {
+                    locationList1.Add(number1);
+                    locationList2.Add(number2);
+                }
+                else
+                {
+                    throw new Exception("Cannot convert Elvish string location to int");
+                }
+            }
+
+            // validations
+            if (locationList1.Count != locationList2.Count)
+            {
+                throw new Exception("Number of elements in each list are not equal, the Elvish Historians messed up");
+            }
+
+            // sort each list
+            locationList1.Sort();
+            locationList2.Sort();
+
+            // solve day 1 problem
+            for (int i = 0; i< locationList1.Count; i++)
+            {
+                // find the difference between each location ID
+                var difference = locationList1[i] - locationList2[i];
+                var tmp = difference < 0 ? difference * -1 : difference;
+                answer += tmp;
+
+                // create a dictionary of the second list for part 2 of the problem
+                if (numberCounts.ContainsKey(locationList2[i]))
+                {
+                    numberCounts[locationList2[i]]++;
+                }
+                else
+                {
+                    numberCounts[locationList2[i]] = 1;
+                }
+            }
+
+            // solve part 2 of day 1 problem
+            foreach (int number in locationList1)
+            {
+                if (numberCounts.TryGetValue(number, out int number2)) 
+                {
+                    var tmp = number * number2;
+                    similarityScore += tmp;
+                }
+            }
+
+            // return the answer
+            Console.WriteLine("List distances: " + answer);
+            Console.WriteLine("Similarity score: " + similarityScore);
+        }
+
+
+    }
+}
---
+++ aoc-2024/main.cs
@@ -0,0 +1,43 @@
+﻿using aoc_2024;
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Enter the number of the AOC day to solve that day's problem: ");
+            if (int.TryParse(Console.ReadLine(), out int choice) && choice > 0 && choice <= 25)
+            {
+                SolveProblem(choice);
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    static void SolveProblem(int problemNumber)
+    {
+        switch (problemNumber)
+        {
+            case 1:
+                // Add code to solve problem 1
+                Console.WriteLine("Solving problem 1...");
+                var day1 = new Day1();
+                day1.EvaluateInput();
+                break;
+            case 2:
+                // Add code to solve problem 2
+                Console.WriteLine("Solving problem 2...");
+                break;
+            // Add more cases as needed
+            default:
+                Console.WriteLine("Problem not implemented yet.");
+                break;
+        }
+    }
+}
